522827654fb9fa7b937e47c76ba8a1acb52a75e1
Hou Kot <chrishouwakot@gmail.com>
2020-04-18 16:27:09 -0700

    Update implement-queue-using-stacks.py

--- implement-queue-using-stacks.py
+++ implement-queue-using-stacks.py
@@ -2,56 +2,27 @@
 class MyQueue:
 
     def __init__(self):
-        """
-        Initialize your data structure here.
-        """
         self.stack1 = []
         self.stack2 = []        
         
 
     def push(self, x: int) -> None:
-        """
-        Push element x to the back of queue.
-        """
         self.stack1.append(x);
         
 
     def pop(self) -> int:
-        """
-        Removes the element from in front of queue and returns that element.
-        """
+        if (len(self.stack2) > 0):
+            return self.stack2.pop()
         while len(self.stack1) > 0: 
             self.stack2.append(self.stack1.pop());        
-        result = self.stack2.pop();
-        while len(self.stack2) > 0:
-            self.stack1.append(self.stack2.pop());
-        return result
+        return self.stack2.pop();
             
 
     def peek(self) -> int:
-        """
-        Get the front element.
-        """
-        while len(self.stack1) > 0: 
-            self.stack2.append(self.stack1.pop());        
-        result = self.stack2.pop();
-        self.stack1.append(result);
-        while len(self.stack2) > 0:
-            self.stack1.append(self.stack2.pop());  
-        return result
+        r = self.pop()
+        self.stack2.append(r);
+        return r;
         
 
     def empty(self) -> bool:
-        """
-        Returns whether the queue is empty.
-        """
-        return len(self.stack1) == 0;
-        
-
-
-# Your MyQueue object will be instantiated and called as such:
-# obj = MyQueue()
-# obj.push(x)
-# param_2 = obj.pop()
-# param_3 = obj.peek()
-# param_4 = obj.empty()
+        return len(self.stack1) + len(self.stack2) == 0;
